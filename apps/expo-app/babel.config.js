53620b8fada9ac7dd3ae3680137be3051cce2964
Rick Love <rick@toldpro.com>
2023-09-29 21:43:19 -0500

    nativewind working for expo web

--- apps/expo-app/babel.config.js
+++ apps/expo-app/babel.config.js
@@ -2,6 +2,14 @@ module.exports = function (api) {
     api.cache(true);
     return {
         presets: [`babel-preset-expo`],
-        plugins: [`nativewind/babel`],
+        plugins: [
+            [
+                `nativewind/babel`,
+                {
+                    // this must be used for expo web so the className passes through
+                    mode: `transformOnly`,
+                },
+            ],
+        ],
     };
 };
