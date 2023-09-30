fecf878b11e9a89581b5fdd70649b0e02a6c22a9
Rick Love <rick@toldpro.com>
2023-09-29 22:47:10 -0500

    expo router working - but broke nativewind

---
+++ apps/expo-app/app/_layout.tsx
@@ -0,0 +1,19 @@
+// import './styles.css';
+
+import { Stack } from 'expo-router/stack';
+
+export default function Layout() {
+    return (
+        <Stack
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: `#f4511e`,
+                },
+                headerTintColor: `#fff`,
+                headerTitleStyle: {
+                    fontWeight: `bold`,
+                },
+            }}
+        />
+    );
+}
---
+++ apps/expo-app/app/index.tsx
@@ -0,0 +1,8 @@
+import { Text } from 'react-native';
+
+import { App } from '../src/app.gen';
+
+export default function Page() {
+  return <App />;
+  // return <Text>Home page 2</Text>;
+}
